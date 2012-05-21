51e54bcbec7866fa70b4a913e45c3d6783fd8c67
Peng Du <du@cs.utk.edu>
2012-05-20 22:05:03 -0400

    On branch dev 	modified:   input1.bib 	modified:   merger.pl

--- merger.pl
+++ merger.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# merger.pl input1.bib input2.bib output.bib -dupcheck 0/1
+# merger.pl input1.bib input2.bib output.bib 
 
 #---- forward declaration ----#
 sub RemoveDuplicates;
@@ -16,7 +16,7 @@ $dupcheck = 0;	# duplication is NOT checked by default
 $| = 1;   # enforce flushing
 
 #---- read input files ----#
-if (scalar @ARGV < 2)
+if (scalar @ARGV < 3)
 {
 	print "ERROR: insufficient arguments\n"; 
 	print "USAGE: merger.pl input1.bib input2.bib output.bib -dupcheck 0/1\n";
@@ -31,17 +31,11 @@ open FH, $ARGV[1];
 @input2 = <FH>;
 close FH;
 
-open FHO, $ARGV[2];
-
-#---- remove duplicates if asked ----#
-
-
 #---- parse input1 and input2 into dictionaries ----#
 %dict1=();
 %dict2=();
-AssembleDict(\@input1, \%dict1);
-AssembleDict(\@input2, \%dict2);
-
+AssembleDict(\@input1, \%dict1, $ARGV[0]);
+AssembleDict(\@input2, \%dict2, $ARGV[1]);
 
 #---- try to add items in dict2 into dict1 ----#
 print "\n";
@@ -95,25 +89,26 @@ while ( ($k, $v) = each %dict2)
 }
 
 
-while ( ($k, $v) = each %dict1) 
+#while ( ($k, $v) = each %dict1) 
+#{
+#	print "$k => \n$v\n";
+#}
+
+
+#---- output result ----#
+open FH, ">$ARGV[2]" or die $!;
+
+$outp = "";
+foreach $k (sort (keys %dict1)) 
 {
-	print "$k => \n$v\n";
+	$outp = $outp.$dict1{$k};
 }
 
+print FH $outp;
 
+close FH;
 
-
-
-open FHO, $ARGV[2];
-
-
-
-
-
-
-
-
-
+print "Result has been written to $ARGV[2]\n";
 
 
 #---- subroutines ----#
@@ -128,6 +123,7 @@ sub AssembleDict
 	$arg = $_[0];
 	my $filelines = join "", @{$arg};
 	$dict = $_[1];
+	$filename = $_[2];
 
 	$filelines =~ s/\r//g;
 
@@ -147,17 +143,59 @@ sub AssembleDict
 	{
 		if ($item =~ /title\s*?=\s*?{{1,2}(.+?)}{1,2}/)
 		{
-			$title = uc $1;
+			$title = lc $1;
 		}
 		else
 		{
-			$title = 'NO_TITLE'.$anoycount;
+			$title = '0NO_TITLE'.$anoycount;
 			$anoycount++;
 		}
 
-		$dict->{$title} = $item;
-	}
+		if (exists $dict->{$title})
+		{
+			print "In file $filename\n\n"; 
+			print color 'bold yellow';
+			print "version 1: \n\n"; 
+			print $dict->{$title};
+			print color 'reset';
+			print ">----------------------------------<\n\n";
+			print color 'bold green';
+			print "version 2: \n\n"; 
+			print $item;
+			print color 'reset';
+			print "collision detected!! select one to keep[1/2]: ";
 
+			$userinput =  <STDIN>;
+			while ($userinput != 1 && $userinput != 2)
+			{
+				print "either 1 or 2, trying again: ";
+				$userinput =  <STDIN>;
+			}
+
+			if ($userinput==2)
+			{
+				delete $dict->{$title};
+				$dict->{$title} = $item;
+				print "\npayload in";
+				print color 'bold red';
+				print " 2 ";
+				print color 'reset';
+				print "is loaded\n\n";
+			}
+			else
+			{
+				print "\npayload in";
+				print color 'bold red';
+				print " 1 ";
+				print color 'reset';
+				print "is loaded\n\n";
+			}
+		}
+		else
+		{
+			$dict->{$title} = $item;
+		}
+	}
 }
 
 
